f1dc3651cccc6b07409064003607daf8d05521aa
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-08 06:09:05 +0530

    Update

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // comment
+    /*
+    multi
+    line
+    comment
+    */
+    std::cout << "I love Garima" << std::endl;
+    std::cout << "She is hawwtttt" << std::endl;
+    return 0;
+}
