90fac1c5143b5450b497672ba44769d6ddcaa136
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-08 06:15:10 +0530

    Updated

---
+++ basics/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int x;      // declaration
+    x = 5;      // assignment
+    int y = 10; // declaration + assignment
+
+    int sum = x + y;
+
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+    std::cout << sum << std::endl;
+
+    return 0;
+}
