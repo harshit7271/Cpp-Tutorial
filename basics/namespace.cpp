96f6a12b4a1a4e0b2b3ef7c47e4973e84ff2484e
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-09 05:39:17 +0530

    Updated

---
+++ basics/namespace.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace first
+{
+    int x = 1;
+}
+
+namespace second
+{
+    int x = 2;
+}
+
+int main()
+{
+    int x = 20;
+    std::cout << x << '\n';
+    std::cout << first::x << '\n';
+    std::cout << second::x << '\n';
+    return 0;
+}
+
+// namespace allows us to assign one or more values to same variable
