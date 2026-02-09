cff9f1099150514f9424af9409cf5713796cf817
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-09 05:30:29 +0530

    Updated

--- basics/const.cpp
+++ basics/const.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    const double pi = 3.14159; // we prefix this using const so that nobody can change it
+    const double PI = 3.14159; // we prefix this using const so that nobody can change it
     double radius = 10;
     double circumference = 2 * pi * radius;
 
