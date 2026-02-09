37071c0110d6d242837a97efae6f036b5b15c4ac
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-09 05:32:18 +0530

    Updated

--- basics/const.cpp
+++ basics/const.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     const double PI = 3.14159; // we prefix this using const so that nobody can change it
+    const int SPEED_OF_LIGHT = 2999792458;
     double radius = 10;
     double circumference = 2 * PI * radius;
 
