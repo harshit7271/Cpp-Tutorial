62f8bfb32c323fdc71fdee9053b321a8847c2de0
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-12 06:18:47 +0530

    Updated

---
+++ Moderate level concepts/if-else.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int age;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    if (age > 18)
+    {
+        std::cout << "You are an adult. " << std::endl;
+    }
+    else if (age == 18)
+    {
+        std::cout << "You just become an adult." << std::endl;
+    }
+    else
+    {
+        std ::cout << "You are a minor.";
+    }
+
+    return 0;
+}
