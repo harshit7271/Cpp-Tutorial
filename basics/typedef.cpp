6d74fad99d327fc5b6f1569c6f42c0095b0d2820
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-12 05:31:24 +0530

    Updated

---
+++ basics/typedef.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;  "std::vector<std::pair<std::string, int">> is data type
+
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
+
+int main()
+{
+    text_t firstName = "Garima";
+    number_t age = 23;
+
+    std::cout << firstName << '\n';
+    std::cout << age << '\n';
+
+    return 0;
+}
